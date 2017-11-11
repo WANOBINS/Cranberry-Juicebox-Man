c41c21d76c891b42c12084f8a0c3b8dd18cd13ee
Cobaltmoon <32678490+Cobaltmoon@users.noreply.github.com>
2017-11-11 17:56:00 -0500

    added Ohno script
    that

---
+++ Assets/Scripts/Ohno.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ohno : MonoBehaviour
+{
+    public AudioSource Source;
+    public AudioClip ohNo;
+    public AudioClip YES;
+    public AudioEchoFilter Echo;
+    private int ohNoCount = 0;
+    public float ohNoDelay;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        Source = GetComponent<AudioSource>();
+        Source.clip = ohNo;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		// for test
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            Source.PlayDelayed(ohNoDelay);
+            ohNoCount++;
+        }
+        if (ohNoCount >= 3)
+        {
+            Source.PlayOneShot(YES);
+            ohNoCount = 0;
+        }
+	}
+}
