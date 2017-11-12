657a9aff1de2a6fb9bae12262927c2d3f03039ec
Steve <thesteveoninja@gmail.com>
2017-11-12 11:56:49 -0500

    Added DeadEnd Script and altered Game Manager

---
+++ Assets/Scripts/DeadEnd.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadEnd : MonoBehaviour {
+
+    public GameObject CranberryJuiceMan;
+    public GameManager GM;
+
+    public AudioClip OhNo1;
+    public AudioClip OhNo2;
+    public AudioClip OhNo3;
+
+	// Use this for initialization
+	void Start () {
+        GM = FindObjectOfType<GameManager>();
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(GM.OhNo1 == false)
+        {
+            // Insert Audio Clip Playing Here
+            GM.OhNo1 = true;
+        }
+
+        if(GM.OhNo1 == true && GM.OhNo2 == false)
+        {
+            // Insert Audio Clip Playing Here
+            GM.OhNo2 = true;
+        }
+
+        if(GM.OhNo1 == true && GM.OhNo2 == true)
+        {
+            // Insert Audio Clip Playing Here
+            // Insert Death Here
+            // Shoebopadoo
+        }
+
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public bool OhNo1;
+    public bool OhNo2;
+
 	// Use this for initialization
 	void Start () {
-		
+
+        OhNo1 = false;
+        OhNo2 = false;
 	}
 	
 	// Update is called once per frame
